f8f03b768e639f9d63ba907955412e64b9fdbc0d
balogun akeem <balogunbiola101@gmail.com>
2023-12-22 16:05:51 +0100

    test

--- pages/dashboard/portfolio.js
+++ pages/dashboard/portfolio.js
@@ -155,7 +155,7 @@ export default function Portfolio() {
                       </span>
                  <p>
                         ₦
-                        {user.calculatedPortfolioValues.calculatedBuyingPower}
+                        {user.calculatedPortfolioValues.calculatedBuyingPower} biola
                         {/* {(
                           user?.portfolio?.buyingPower +
                           (user?.portfolio?.gameId?.allowTradingWithMargin
@@ -177,7 +177,7 @@ export default function Portfolio() {
 
                       <p>
                         ₦
-                        {user.calculatedPortfolioValues.calculatedCash}
+                        {user.calculatedPortfolioValues.calculatedCash} balo
                         {/* {(
                           user?.portfolio?.cash 
                         )
