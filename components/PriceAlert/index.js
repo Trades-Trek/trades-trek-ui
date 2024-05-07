c4de55fd39e4c469a611de10f67f6f4931c8574b
balogun biola <balogunbiola101@gmail.com>
2024-05-07 21:30:05 +0100

    style improvements

--- components/PriceAlert/index.js
+++ components/PriceAlert/index.js
@@ -90,7 +90,7 @@ export default function WatchListData() {
         {!isLoading ? (
           watchListData.length > 0 ? (
             !showWatchList ? (
-              <div className="wrapper--text flex">
+              <div className="wrapper--text flex gap-5">
                 {watchListData.map((data, index) => {
                   if (index <= 3) {
                     return (
