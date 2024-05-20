ba64d3ab837e17ab05494f410a607a9dbbf6a32e
balogun biola <balogunbiola101@gmail.com>
2024-05-20 20:02:24 +0100

    new fix

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
 
   function authCheck(url) {
     // redirect to login page if accessing a private page and not logged in
-    const publicPaths = ['/login', '/', '/sign-up/', '/otp/', '/privacy-policy/', '/terms/', '/campaign-signup/'];
+    const publicPaths = ['/login', '/', '/sign-up/', '/otp/', '/privacy-policy/', '/terms/', '/campaign-signup/', '/download-app/'];
     const path = url.split('?')[0];
     if (!userService.userValue && !publicPaths.includes(path)) {
       setAuthorized(false);
