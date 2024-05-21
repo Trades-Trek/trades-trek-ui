bc460b9bba8b13bd220307dc303e1aa293b252ca
balogun biola <balogunbiola101@gmail.com>
2024-05-21 13:41:13 +0100

    improve otp page

--- pages/otp.js
+++ pages/otp.js
@@ -3,7 +3,7 @@ import Footer from '../components/Footer/Footer';
 import Otp from '../components/Otp/Otp';
 export default function OtpProcess() {
   useEffect(() => {
-    document.body.classList.add('otp--page h-screen bg-[#E6CCFF]');
+    document.body.classList.add('otp--page');
     document.body.classList.remove('signUp--page');
   });
   return (
