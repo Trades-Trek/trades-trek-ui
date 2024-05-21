d50679ab9524345ead0a8398f341f5a85ccb7f12
balogun biola <balogunbiola101@gmail.com>
2024-05-21 15:29:00 +0100

    add congratulations text

--- pages/download-app/index.js
+++ pages/download-app/index.js
@@ -8,6 +8,13 @@ const DownloadApp = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-y-7 md:flex-row bg-palette-one gap-x-14 h-screen">
       <div className="md:w-[579px] px-3 md:px-0">
+
+      <h2 className="font-bold text-[32px] text-center md:text-left mb-2 pt-12 md:pt-0 text-[#8000ff]">
+          Congratulations
+        </h2>
+        <p className=" mb-8 text-lg font-light text-center md:text-left leading-[25px] text-[#8000ff]">
+          Your Trades Trek account has been successfully created.
+        </p>
         <br />
 
         <h2 className="font-bold text-[32px] text-center md:text-left mb-2 pt-12 md:pt-0">
